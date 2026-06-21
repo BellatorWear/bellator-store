1518370a3983ea0d69efc357f1bb67b766625253
max <wuehrmax06@gmail.com>
2026-06-21 13:04:26 +0200

    warenkorb 2

--- app/shop/components/AddToCartButton.tsx
+++ app/shop/components/AddToCartButton.tsx
@@ -4,12 +4,9 @@ import { handleAction } from '../../actions';
 import styles from './ProductCard.module.css';
 
 export default function AddToCartButton({ productId }: { productId: string }) {
-  // Wir definieren die Action als direkte Form-Action
-  // Next.js erwartet, dass die Funktion, die an action übergeben wird, 
-  // die FormData als erstes Argument akzeptiert.
-  
   return (
-    <form action={handleAction}>
+    // Der Wrapper () => handleAction(formData) löst das Problem
+    <form action={async (formData) => { await handleAction(formData) }}>
       <input type="hidden" name="actionType" value="addToCart" />
       <input type="hidden" name="productId" value={productId} />
       
